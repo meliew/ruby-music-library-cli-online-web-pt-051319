e7777c27b52c34356e97a0b74b7017430bfd9727
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-21 19:22:05 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,13 +1,13 @@
 require 'pry'
 class MusicLibraryController
-
-
-
+  
+  
+  
   def initialize(path= "./db/mp3s")
     MusicImporter.new(path).import
   end
-
-
+  
+  
   def call
     loop do
       puts "Welcome to your music library!"
@@ -20,7 +20,7 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       answer = gets.chomp
-
+      
       case answer
       when "list songs"
         self.list_songs
@@ -40,54 +40,54 @@ class MusicLibraryController
         break
       else
         "Type in a valid request please"
-end
+      end
       # if answer == 'exit'
       #   break
-      end
     end
+  end
   
-
+  
   def list_songs
     Song.all.sort_by(&:name).each.with_index(1) do |song, i|
       puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
-
+  
   def list_artists
     Artist.all.sort_by(&:name).each.with_index(1) do |art, idx|
       puts "#{idx}. #{art.name}"
     end
   end
-
+  
   def list_genres
     Genre.all.sort_by(&:name).each.with_index(1) do |gen, idx|
       puts "#{idx}. #{gen.name}"
     end
   end
-
+  
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.chomp
-
+    
     if artist = Artist.find_by_name(input)
       artist.songs.sort_by(&:name).each.with_index(1) do |song, idx|
         puts "#{idx}. #{song.name} - #{song.genre.name}"
       end
-
+      
     end
   end
-
+  
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets.chomp
-
+    
     if genre = Genre.find_by_name(input)
       genre.songs.sort_by(&:name).each.with_index(1) do |song, ind|
         puts "#{ind}. #{song.artist.name} - #{song.name}"
       end
     end
   end
-
+  
   def play_song
     puts "Which song number would you like to play?"
     input = gets.chomp.to_i
@@ -99,7 +99,7 @@ end
     end
     puts "Playing #{song.name} by #{song.artist.name}" if song
   end
-
+  
 end
 
 # if input == a matching song
